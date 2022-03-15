613d2fd8d994d0ae5f8870a9bdb99e7654bc046c
goarnaiz <72554806+goarnaiz@users.noreply.github.com>
2022-03-15 13:43:54 +0100

    Add files via upload

---
+++ app/src/main/java/com/example/busschedule/database/Schedule.kt
@@ -0,0 +1,8 @@
+package com.example.busschedule.database
+
+@Entity
+data class Schedule(
+    @PrimaryKey val id: Int,
+    @NonNull @ColumnInfo(name = "stop_name") val stopName: String,
+    @NonNull @ColumnInfo(name = "arrival_time") val arrivalTime: Int
+)
